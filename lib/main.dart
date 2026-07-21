c802b17590127cf453fa6004badb283ecd4f66d8
tosif121 <tosifraza8867@gmail.com>
2026-07-21 20:48:20 +0530

    fix: request RECORD_AUDIO system permission for WebView WebRTC calls
    - Add Permission.microphone and Permission.camera runtime requests
      via permission_handler (system-level RECORD_AUDIO needed by WebView
      Chrome engine for getUserMedia)
    - Set Android WebView settings: mixedContentMode=alwaysAllow,
      mediaPlaybackRequiresUserGesture=false, allowFileAccess=true,
      Chrome user agent
    - Auto-grant WebView permission requests (camera/microphone)
    - Inject JS to override Notification.requestPermission (prevent dialog)
    - Enable WebView remote debugging
    - Pipe WebView console logs to logcat

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_android/webview_flutter_android.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'services/fcm_service.dart';
 
 Future<void> main() async {
@@ -8,6 +10,12 @@ Future<void> main() async {
 
   await Firebase.initializeApp();
   await FcmService().init();
+  await AndroidWebViewController.enableDebugging(true);
+
+  await [
+    Permission.microphone,
+    Permission.camera,
+  ].request();
 
   runApp(const SamvaadApp());
 }
@@ -38,14 +46,53 @@ class _WebViewScreenState extends State<WebViewScreen> {
   @override
   void initState() {
     super.initState();
-    _controller = WebViewController()
+
+    const notificationJs = '''
+(function() {
+  var originalRequest = Notification.requestPermission;
+  Notification.requestPermission = function() {
+    if (originalRequest) {
+      try { originalRequest.call(Notification); } catch(_) {}
+    }
+    return Promise.resolve('granted');
+  };
+  Notification.permission = 'granted';
+})();
+''';
+
+    _controller = WebViewController(
+      onPermissionRequest: (request) {
+        request.grant();
+      },
+    )
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..setNavigationDelegate(
         NavigationDelegate(
+          onPageStarted: (url) {
+            _controller.runJavaScript(notificationJs);
+          },
           onPageFinished: (_) => setState(() {}),
         ),
       )
       ..loadRequest(Uri.parse('https://devapp.iotcom.io/webphone/v1/'));
+
+    _configureAndroidSettings();
+  }
+
+  Future<void> _configureAndroidSettings() async {
+    if (_controller.platform is AndroidWebViewController) {
+      final android = _controller.platform as AndroidWebViewController;
+      await android.setMediaPlaybackRequiresUserGesture(false);
+      await android.setMixedContentMode(MixedContentMode.alwaysAllow);
+      await android.setAllowFileAccess(true);
+      await android.setUserAgent(
+        'Mozilla/5.0 (Linux; Android 14; Pixel 8 Pro) AppleWebKit/537.36 '
+        '(KHTML, like Gecko) Chrome/124.0.6367.113 Mobile Safari/537.36',
+      );
+      await android.setOnConsoleMessage((message) {
+        debugPrint('[WEBVIEW:${message.level.name}] ${message.message}');
+      });
+    }
   }
 
   @override
